d5222b7edba8ca0bf914d88bb00f3a768db940ac
Vlad Rabchuk <werehere2121@gmail.com>
2022-11-09 15:36:22 +0300

    Task2 and Task2 Draft

--- Rabchuk-Vladislav-Lesson-3/Task2Draft.js
+++ Rabchuk-Vladislav-Lesson-3/Task2Draft.js
@@ -1,8 +1,25 @@
 const CreatePerson = function (name, skills) {
   this.name = name;
   this.skills = skills;
+
+  // Never put a method here because this makes every object carry this function. Instead we're gonna add this method to the prototype
+  // this.description = function() {
+  //   console.log(`${name} `);
+  // }
 };
 
+const harry = new CreatePerson('Harry Potter', [
+  'farting',
+  'wizardry',
+  'kindness',
+]);
+
+// 1 New {} is created
+// 2 function is called and its this is set to {}
+// 3 {} is linked to prototype
+// 4 function automatically returns {}
+
+// Prototypes
 CreatePerson.prototype.addSkill = function (skill) {
   const arr = this.skills;
   if (!arr.includes(skill)) {
