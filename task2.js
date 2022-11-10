4fb21d57752ae2354aedbecea3bf49414d4fba37
EmelyanovVlad <haos1996haos@gmail.com>
2022-11-10 14:08:16 +0300

    added method call chain

--- task2.js
+++ task2.js
@@ -5,14 +5,17 @@ function createPerson(name, skills) {
     addSkill(value) {
       this.skills.push(value);
       this.skills = [...new Set(this.skills)];
+      return this;
     },
     removeSkill(value) {
       this.skills.filter((item, i) => {
         if (item === value) this.skills.splice(i, 1);
       });
+      return this;
     },
     setName(value) {
       this.name = value;
+      return this;
     },
   };
 }
