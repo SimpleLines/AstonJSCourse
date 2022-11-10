08a3704b94a979ccbd876a1dbe912574fdc36945
Romanovskiy Vladislav <theromanovsky@gmail.com>
2022-11-10 17:00:24 +0300

    lesson3

---
+++ task1.js
@@ -0,0 +1,21 @@
+Array.prototype.filterArray = function (callback, thisArg) {
+  if (typeof callback !== 'function') throw new Error('Callback is not a function');
+  let context = thisArg ?? this;
+
+  let res = [];
+
+  for (let i = 0; i < this.length; i++) {
+    if (this[i] === undefined) {
+      continue;
+    }
+
+    let current = callback.call(context, this[i], i, this);
+    
+    if (current) {
+      res.push(this[i]);
+    }
+  }
+
+  return res;
+};
+
