6afaecd29600a7e66cd4926265ec6fc62663aad7
Vlad Rabchuk <werehere2121@gmail.com>
2022-11-11 13:47:37 +0300

    Created one function

--- Task2.js
+++ Task2.js
@@ -1,24 +1,26 @@
-const CreatePerson = function (name, skills) {
-  this.name = name;
-  this.skills = skills;
-};
-
-CreatePerson.prototype.addSkill = function (skill) {
-  const arr = this.skills;
-  if (!arr.includes(skill)) {
-    arr.push(skill);
-  }
-};
-
-CreatePerson.prototype.removeSkill = function (skill) {
-  if (skill) {
-    const i = this.skills.indexOf(skill);
-    this.skills.splice(i, 1);
-  } else {
-    this.skills.pop();
-  }
-};
-
-CreatePerson.prototype.setName = function (name) {
-  this.name = name;
-};
+function createPerson(name, skills) {
+  return {
+    name,
+    skills: [],
+    addSkill(skill) {
+      const arr = this.skills;
+      if (!arr.includes(skill)) {
+        arr.push(skill);
+      }
+      return this;
+    },
+    removeSkill(skill) {
+      if (skill) {
+        const i = this.skills.indexOf(skill);
+        this.skills.splice(i, 1);
+      } else {
+        this.skills.pop();
+      }
+      return this;
+    },
+    setName(name) {
+      this.name = name;
+      return this;
+    },
+  };
+}
