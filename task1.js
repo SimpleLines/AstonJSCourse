f6bd4dc32c9e28838d000f2f504b52307b84a72d
Vladislav Sheftor <vladislav-shetor@yandex.ru>
2022-11-12 13:58:12 +0300

    Lesson1 tasks correction complited

--- task1.js
+++ task1.js
@@ -2,7 +2,5 @@ function sum(a, b) {
   if (Number.isFinite(+a) === false || Number.isFinite(+b) === false) {
     throw Error('Переданный аргумент не может быть приведен к конечному числу');
   }
-  return +(+a + +b).toFixed(3); // 1 + 2 = 3 без нулей
+  return Number((+a + +b).toFixed(3));
 }
-
-console.log(sum(1, 2));
--- task2.js
+++ task2.js
@@ -1,7 +1,7 @@
 function getNumberRadix(number, radix) {
   number = Number(number);
 
-  if (!(Number.isInteger(number) && number > 0 && Number.isInteger(radix) && radix >= 2 && radix <= 16)) {
+  if (!(Number.isInteger(number) && Number.isInteger(radix) && number > 0 && radix >= 2 && radix <= 16)) {
     throw Error('Функция getNumberRadix была вызвана с некорректными параметрами');
   }
   return number.toString(radix);
