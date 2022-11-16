495ea521ef157c6cc81cc34fd139733d6f1bf8e7
Egor Vladykin <lotov.97@list.ru>
2022-11-16 23:37:30 +0300

    true solution 2

--- task4.js
+++ task4.js
@@ -1,7 +1,7 @@
 function getUniqArray(arr){
   let res = Array.from(new Set(arr));
   res.forEach((i) =>{
-    if (typeof i !== 'number' || typeof i == Infinity || typeof i == NaN){
+    if (typeof i !== 'number' || typeof i == isFinite || typeof i == NaN){
       throw Error('В getUniqArray был передан невалидный параметр. Аргумент arr должен быть массивом чисел');
     }
     })
