d1e2a7539f43ca460cb90749280e0e03e8b0bc7e
EmelyanovVlad <haos1996haos@gmail.com>
2022-11-25 01:52:09 +0300

    created homework №6

--- task1/main.js
+++ task1/main.js
@@ -1,36 +1,44 @@
 const entriesPage = 10;
-const entriesGroup = 'todos';
-const wrap = document.getElementById('wrapper');
-const buttonNext = document.querySelector('.button__next');
-const buttonBack = document.querySelector('.button__back');
-let activePage = 1;
+const entriesGroup = 'comments';
+let activePage = location.search.substring(17);
 
-buttonNext.addEventListener('click', () => {
-  activePage++;
+const wrap = document.getElementById('pagination__buttons-page');
+const buttonNext = document.querySelector('.pagination__button-next');
+const buttonBack = document.querySelector('.pagination__button-back');
+const wrapperContent = document.querySelector('.wrapper__content');
 
-  getPosts(
-    `https://jsonplaceholder.typicode.com/${entriesGroup}?_limit=${entriesPage}&_page=${activePage}`
-  ).then((result) => {
-    if (result.total / entriesPage < activePage) {
-      activePage--;
-    } else {
-      console.log(result.data);
-      onClickPageActive(activePage);
-    }
+const createContent = (data) => {
+  data.map((item) => {
+    const cardContent = document.createElement('div');
+    const id = document.createElement('p');
+    const name = document.createElement('h2');
+    const email = document.createElement('p');
+    const body = document.createElement('p');
+
+    cardContent.classList.add('content-card');
+    id.classList.add('card-id');
+    name.classList.add('card-name');
+    name.classList.add('card-name-full');
+    email.classList.add('card-email');
+    body.classList.add('card-body');
+
+    id.textContent = `Id: ${item.id}`;
+    name.textContent = `Name: ${item.name}`;
+    email.textContent = `Email: ${item.email}`;
+    body.textContent = `Body: ${item.body}`;
+
+    wrapperContent.append(cardContent);
+
+    cardContent.append(name);
+    cardContent.append(email);
+    cardContent.append(body);
+    cardContent.append(id);
+
+    name.addEventListener('click', () => {
+      name.classList.toggle('card-name-full');
+    });
   });
-});
-
-buttonBack.addEventListener('click', () => {
-  activePage == 1 ? activePage : activePage--;
-  onClickPageActive(activePage);
-
-  getPosts(
-    `https://jsonplaceholder.typicode.com/${entriesGroup}?_limit=${entriesPage}&_page=${activePage}`
-  ).then((result) => {
-    //написать как и на кнопке некст
-    console.log(result.data);
-  });
-});
+};
 
 const getPosts = async (url) => {
   try {
@@ -46,6 +54,35 @@ const getPosts = async (url) => {
   }
 };
 
+buttonNext.addEventListener('click', () => {
+  if (!location.search.length) activePage = 1;
+  activePage++;
+  onClickGetRequest(-1);
+});
+
+buttonBack.addEventListener('click', () => {
+  if (!location.search.length) activePage = 1;
+  activePage--;
+  onClickGetRequest(+1);
+});
+
+const onClickGetRequest = (action) => {
+  getPosts(`https://jsonplaceholder.typicode.com/${entriesGroup}?_limit=${entriesPage}&_page=${activePage}`).then(
+    (result) => {
+      if (activePage > result.total / entriesPage || activePage == 0) {
+        activePage = activePage + action;
+      } else {
+        while (wrapperContent.firstChild) {
+          wrapperContent.removeChild(wrapperContent.firstChild);
+        }
+        createContent(result.data);
+        onClickPageActive(activePage);
+        saveParamsUrl(activePage);
+      }
+    }
+  );
+};
+
 const errorConnection = () => {
   const error = document.createElement('p');
   error.textContent = 'Сервер недоступен 😔';
@@ -54,11 +91,12 @@ const errorConnection = () => {
 
 const createPagination = (result) => {
   if (result.total) {
-    const pagesAmount = result.total / 10;
+    const pagesAmount = Math.ceil(result.total / entriesPage);
 
     for (let i = 1; i <= pagesAmount; i++) {
       const numPage = document.createElement('button');
-      if (i === 1) numPage.classList.add('page_active');
+      if (!location.search.length && i == 1) numPage.classList.add('page_active');
+      if (i == activePage) numPage.classList.add('page_active');
       numPage.classList.add('button__pagination');
       numPage.textContent = i;
       wrap.append(numPage);
@@ -67,9 +105,13 @@ const createPagination = (result) => {
         getPosts(
           `https://jsonplaceholder.typicode.com/${entriesGroup}?_limit=${entriesPage}&_page=${numPage.textContent}`
         ).then((result) => {
-          console.log(result.data); //вывод данных при клике на пагинации
+          while (wrapperContent.firstChild) {
+            wrapperContent.removeChild(wrapperContent.firstChild);
+          }
+          createContent(result.data);
           activePage = i;
           onClickPageActive(numPage.textContent);
+          saveParamsUrl(numPage.textContent);
         });
       });
     }
@@ -81,18 +123,19 @@ const createPagination = (result) => {
 };
 
 const onClickPageActive = (numPage) => {
-  console.log(numPage);
   const pages = document.querySelectorAll('.button__pagination');
   for (let page of pages) {
-    numPage == page.textContent
-      ? page.classList.add('page_active')
-      : page.classList.remove('page_active'); //исправить активную страницу
+    numPage == page.textContent ? page.classList.add('page_active') : page.classList.remove('page_active');
   }
 };
 
-getPosts(
-  `https://jsonplaceholder.typicode.com/${entriesGroup}?_limit=${entriesPage}&_page=1`
-).then((result) => {
-  console.log(result.data); //вывод данных при первом рендере
+const saveParamsUrl = (activePage) => {
+  history.replaceState(null, null, `?_limit=${entriesPage}&_page=${activePage}`);
+};
+
+if (!location.search.length) location.search = `?_limit=${entriesPage}&_page=${1}`;
+
+getPosts(`https://jsonplaceholder.typicode.com/${entriesGroup}${location.search}`).then((result) => {
+  createContent(result.data);
   createPagination(result);
 });
