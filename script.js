e7aad20722896504ceee817eebeb36765f61bc94
Egor Vladykin <lotov.97@list.ru>
2022-11-25 21:54:49 +0300

    refactoring les 7

--- script.js
+++ script.js
@@ -1,155 +1,40 @@
-const hoursElement = document.querySelector('.hour');
-const minutesElement = document.querySelector('.minute');
-const secondsElement = document.querySelector('.second');
-const millisecondsElement = document.querySelector('.millisecond');
-const resultsElement = document.querySelector('.result');
+var h1 = document.getElementsByTagName('h1')[0];
+var start = document.getElementById('start');
+var stop = document.getElementById('stop');
+var reset = document.getElementById('reset');
+var sec = 0;
+var min = 0;
+var hrs = 0;
+var t;
 
-const startBtn = document.querySelector('.start');
-const pauseBtn = document.querySelector('.pause');
-const stopBtn = document.querySelector('.stop');
-const lapBtn = document.querySelector('.lap');
-
-let hours = 00;
-let minutes = 00;
-let seconds = 00;
-let milliseconds = 00;
-let interval = null;
-
-const localMilliSec = localStorage.getItem('milliseconds');
-const localSeconds = localStorage.getItem('seconds');
-const localMinutes = localStorage.getItem('minutes');
-const localHours = localStorage.getItem('hours');
-const localResults = localStorage.getItem('results');
-
-if (localResults) {
-	resultsElement.innerHTML = JSON.parse(localResults);
+function tick(){
+    sec++;
+    if (sec >= 60) {
+        sec = 0;
+        min++;
+        if (min >= 60) {
+            min = 0;
+            hrs++;
+        }
+    }
+}
+function add() {
+    tick();
+    h1.textContent = (hrs > 9 ? hrs : "0" + hrs) 
+        	 + ":" + (min > 9 ? min : "0" + min)
+       		 + ":" + (sec > 9 ? sec : "0" + sec);
+    timer();
+}
+function timer() {
+    t = setTimeout(add, 1000);
 }
 
-if (localMilliSec) {
-	milliseconds = +localMilliSec;
-	seconds = +localSeconds;
-	minutes = +localMinutes;
-	hours = +localHours;
-	millisecondsFunc();
-	secondsFunc();
-	minutesFunc();
-	hoursFunc();
+timer();
+start.onclick = timer;
+stop.onclick = function() {
+    clearTimeout(t);
 }
-
-if (localResults) {
-	resultsElement.innerHTML = JSON.parse(localResults);
-}
-
-startBtn.addEventListener('click', () => {
-	clearInterval(interval);
-	interval = setInterval(startTimer, 10);
-});
-pauseBtn.addEventListener('click', () => {
-	clearInterval(interval);
-});
-stopBtn.addEventListener('click', () => {
-	clearInterval(interval);
-	clearFields();
-	resultsElement.innerHTML = '';
-	localStorage.clear();
-});
-lapBtn.addEventListener('click', () => {
-	lapFunc();
-});
-
-function startTimer() {
-	milliseconds++;
-	millisecondsFunc();
-	secondsFunc();
-	minutesFunc();
-	hoursFunc();
-	localStorage.setItem('milliseconds', milliseconds);
-	localStorage.setItem('seconds', seconds);
-	localStorage.setItem('minutes', minutes);
-	localStorage.setItem('hours', hours);
-}
-
-function clearFields() {
-	hours = 00;
-	minutes = 00;
-	seconds = 00;
-	milliseconds = 00;
-	hoursElement.textContent = '00';
-	secondsElement.textContent = '00';
-	minutesElement.textContent = '00';
-	millisecondsElement.textContent = '00';
-}
-
-function millisecondsFunc() {
-	if (milliseconds < 9) {
-		millisecondsElement.textContent = '0' + milliseconds;
-	}
-	if (milliseconds > 9) {
-		millisecondsElement.textContent = milliseconds;
-	}
-	if (milliseconds > 99) {
-		seconds++;
-		secondsElement.textContent = '0' + seconds;
-		milliseconds = 0;
-		millisecondsElement.textContent = '0' + milliseconds;
-	}
-}
-
-function secondsFunc() {
-	if (seconds < 9) {
-		secondsElement.textContent = '0' + seconds;
-	}
-	if (seconds > 9) {
-		secondsElement.textContent = seconds;
-	}
-	if (seconds > 59) {
-		minutes++;
-		minutesElement.textContent = '0' + minutes;
-		seconds = 0;
-		secondsElement.textContent = '0' + seconds;
-	}
-}
-
-function minutesFunc() {
-	if (minutes < 9) {
-		minutesElement.textContent = '0' + minutes;
-	}
-	if (minutes > 9) {
-		minutesElement.textContent = minutes;
-	}
-	if (minutes > 59) {
-		hours++;
-		hoursElement.textContent = '0' + hours;
-		minutes = 0;
-		minutesElement.textContent = '0' + minutes;
-	}
-}
-
-function hoursFunc() {
-	if (hours < 9) {
-		hoursElement.textContent = '0' + hours;
-	}
-	if (hours > 9) {
-		hoursElement.textContent = hours;
-	}
-	if (hours == 24) {
-		millisecondsElement.textContent = 00;
-		secondsElement.textContent = 00;
-		minutesElement.textContent = 00;
-		hoursElement.textContent = 00;
-	}
-}
-
-function lapFunc() {
-	const block = document.createElement('div');
-	block.textContent = `Result: ${hours < 9 ? '0' + hours : hours}:${
-		minutes < 9 ? '0' + minutes : minutes
-	}:${seconds < 9 ? '0' + seconds : seconds}:${
-		milliseconds < 9 ? '0' + milliseconds : milliseconds
-	}`;
-	resultsElement.append(block);
-	localStorage.setItem(
-		'results',
-		JSON.stringify(resultsElement.innerHTML)
-	);
+reset.onclick = function() {
+    h1.textContent = "00:00:00";
+    seconds = 0; minutes = 0; hours = 0;
 }
