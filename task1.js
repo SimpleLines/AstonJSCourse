4327ae6b350c9a9c9d2d986d70bc24235b95154d
vlad <108489036+border-none@users.noreply.github.com>
2022-11-27 06:41:40 +0300

    Rabchuk Vladislav: Lesson 3: Task 1, 2 (#28)
    * Lesson 3 Task 2 Draft
    
    * Task2 and Task2 Draft
    
    * Task1 and Task1Draft
    
    * Update Task1.js
    
    * Removed unnecessary folder
    
    * Created one function
    
    * Deleted drafts
    
    * renaming from Task to task
    
    * Final
    
    * Updated

---
+++ task1.js
@@ -0,0 +1,9 @@
+Array.prototype.filterArray = function (cb) {
+  const result = [];
+  for (const i of this.keys()) {
+    if (cb(this[i], i, this)) {
+      result.push(this[i]);
+    }
+  }
+  return result;
+};
